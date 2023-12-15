0f1c7dd80ef2198952bbc5ce36890ab506c1f454
Cato Skogholt <cato@nhi.no>
2023-12-15 11:13:53 +0100

    add todo table

---
+++ prisma/migrations/20231215100211_todos_added/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT,
+    "content" TEXT,
+    "authorId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
