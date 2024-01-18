477aa0e9fc0ec785a26c48f1db3caf2398389774
Cato Skogholt <cskogholt@gmail.com>
2024-01-18 15:38:10 +0100

    Refactor useDebounce hook to include credit

--- src/lib/use-debounce.ts
+++ src/lib/use-debounce.ts
@@ -2,13 +2,14 @@ import { useEffect, useRef } from "react"
 
 type Timer = ReturnType<typeof setTimeout>
 type SomeFunction = (...args: any[]) => void
+
 /**
- *
+ * A hook that returns a debounced version of the function passed to it
+ * Credit: https://medium.com/@philip.andrewweedewang/debounce-hook-in-react-typescript-in-20-lines-of-code-9cde26254d10
  * @param func The original, non debounced function (You can pass any number of args to it)
  * @param delay The delay (in ms) for the function to return
  * @returns The debounced function, which will run only if the debounced function has not been called in the last (delay) ms
  */
-
 export function useDebounce<Func extends SomeFunction>(
   func: Func,
   delay = 1000
